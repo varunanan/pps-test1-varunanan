96b67db7889d1f69f643236dbb184b1fb7a2f15c
Varunanand <94883501+varunanan@users.noreply.github.com>
2021-12-12 14:55:40 +0000

    done

--- p10final.c
+++ p10final.c
@@ -1,1 +1,25 @@
-
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("enter a number\n");
+  scanf("&d", &a);
+return a;
+}
+int add(int a, int b)
+{
+  int c;
+  c=a+b;
+  return c;
+}
+void output(int a, int b, int c)
+{
+  printf("%d + %d is %d\n", a, b, c);
+}
+int main()
+{
+  int a=input();
+  int b=input();
+  int c=add(a,b);
+output(a,b,c);
+}
