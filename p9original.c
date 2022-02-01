4201fbf4ee2ae20ff46e900e47aa5bce57618299
Varunanand <94883501+varunanan@users.noreply.github.com>
2022-02-01 15:17:03 +0000

    p9 done

--- p9original.c
+++ p9original.c
@@ -1,1 +1,55 @@
- 
+ #include <stdio.h>
+
+
+void input(char *a, char *b);
+int compare(char *a, char *b);
+void output(char *a, char *b, int ans);
+
+int main()
+{
+  char a[20], b[20];
+  int ans;
+
+  input(a, b);
+  ans = compare(a, b);
+  output(a, b, ans);
+
+  return 0;
+}
+
+void input(char *a, char *b)
+{
+    printf("Enter a string a: ");
+    scanf("%s", a);
+
+    printf("Enter a string b: ");
+    scanf("%s", b);
+}
+
+
+//int compare(char a[],char b[])
+
+int compare(char *a, char *b)
+{
+    int flag=0, i=0;  // integer variables declaration
+
+    for(i=0; a[i] && (a[i] == b[i]); i++);
+        int ind_dif = a[i] - b[i] ;
+
+
+    return ind_dif;
+}
+
+
+
+void output(char *a, char *b, int ans)
+{
+    if(ans < 0)
+      printf("%s is greater than %s\n", b, a);
+
+    else if(ans > 0)
+      printf("%s is greater than %s\n", a, b);
+
+    else
+       printf("%s is equal to %s\n", b, a);
+}
